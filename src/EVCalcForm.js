ecd008b684795c60d2050af681bfe4657e8411f2
Cu3PO42 <cu3po42@gmail.com>
2018-03-24 21:20:28 +0100

    Add footer

--- src/EVCalcForm.js
+++ src/EVCalcForm.js
@@ -26,63 +26,68 @@ export default class EVCalcForm extends React.Component {
     render() {
       return (
         <div className="mainWrapper">
-          <h1>EV Battle Calculator</h1>
-          <label>
-            Has Pokérus
-            <input
-              name="hasPkrs"
-              type="checkbox"
-              checked={this.state.hasPkrs}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-          <label>
-            Has Power Item
-            <input
-              name="hasPwrItem"
-              type="checkbox"
-              checked={this.state.hasPwrItem}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-          <label>
-            Beginning EVs
-            <input
-              name="startingEVs"
-              type="number"
-              value={this.state.startingEVs}
-              onChange={this.handleChange}
-              min="0"
-              max={this.state.targetEVs}
-            />
-          </label>
-          <br />
-          <label>
-            Target EVs
-            <input
-              name="targetEVs"
-              type="number"
-              value={this.state.targetEVs}
-              onChange={this.handleChange}
-              min="0"
-              max="252"
-            />
-          </label>
-          <br />
-          <label>
-            EV Yield
-            <select
-              name="evYield"
-              value={this.state.evYield}
-              onChange={this.handleChange}
-            >
-              <option value="one">1</option>
-              <option value="two">2</option>
-            </select>
-          </label>
-          <EVCalcResults evCalcResultData={this.state} />
+          <div className="main">
+            <h1>EV Battle Calculator</h1>
+            <label>
+              Has Pokérus
+              <input
+                name="hasPkrs"
+                type="checkbox"
+                checked={this.state.hasPkrs}
+                onChange={this.handleChange}
+              />
+            </label>
+            <br />
+            <label>
+              Has Power Item
+              <input
+                name="hasPwrItem"
+                type="checkbox"
+                checked={this.state.hasPwrItem}
+                onChange={this.handleChange}
+              />
+            </label>
+            <br />
+            <label>
+              Beginning EVs
+              <input
+                name="startingEVs"
+                type="number"
+                value={this.state.startingEVs}
+                onChange={this.handleChange}
+                min="0"
+                max={this.state.targetEVs}
+              />
+            </label>
+            <br />
+            <label>
+              Target EVs
+              <input
+                name="targetEVs"
+                type="number"
+                value={this.state.targetEVs}
+                onChange={this.handleChange}
+                min="0"
+                max="252"
+              />
+            </label>
+            <br />
+            <label>
+              EV Yield
+              <select
+                name="evYield"
+                value={this.state.evYield}
+                onChange={this.handleChange}
+              >
+                <option value="one">1</option>
+                <option value="two">2</option>
+              </select>
+            </label>
+            <EVCalcResults evCalcResultData={this.state} />
+          </div>
+          <footer>
+            Made with ❤ by bumba and Cu3PO42
+          </footer>
         </div>
       );
     }
